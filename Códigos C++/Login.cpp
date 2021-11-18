9bf6e7fa4a5d57553c9bfd2a8e49e0568febfe12
Pedro Hernández <71133442+PedroHernandez654@users.noreply.github.com>
2021-11-18 15:05:59 -0600

    Update Login.cpp

--- Códigos C++/Login.cpp
+++ Códigos C++/Login.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main ()
 {
-    int clave = 0, intentos=0;
+    int clave = 0, intentos=0;// declaramos variables para ingreso
     do {
         cout << "Ingresa tu clave de acceso:" << endl;
-        cin >> clave;
+        cin >> clave;//leemos la clave
         intentos++;
-        if (clave !=1234){
+        if (clave !=1234){// intentos
             cout << "clave incorrecta" << endl;
         }
     }while (clave!=1234 && intentos<3);
