1c0b1d5da89925f7c1212e42d91103e958076bc4
Pedro Hernández <71133442+PedroHernandez654@users.noreply.github.com>
2022-12-05 07:11:26 -0600

    Clases
    Archivos de clases

---
+++ Examen2/Clases/folio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen2Parcial
+{
+    public class folio
+    {
+        int No_folio, id_conductorr, id_vehiculoo;
+        string direccion, Estado_entrega, id_folio;
+
+        public int No_folio1 { get => No_folio; set => No_folio = value; }
+        public int Id_conductorr { get => id_conductorr; set => id_conductorr = value; }
+        public int Id_vehiculoo { get => id_vehiculoo; set => id_vehiculoo = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Estado_entrega1 { get => Estado_entrega; set => Estado_entrega = value; }
+        public string Id_folio { get => id_folio; set => id_folio = value; }
+    }
+}
