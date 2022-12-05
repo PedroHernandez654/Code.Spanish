da8cb83bf4b3cf54473b059b82fe372b26e4b500
Pedro Hernández <71133442+PedroHernandez654@users.noreply.github.com>
2022-12-05 07:33:21 -0600

    Add files via upload

---
+++ Algoritmo_Dijkstra/Algoritmo_Dijkstra/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algoritmo_Dijkstra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Ninicio = 0, Nfinal = 0, Distancia = 0, CantNodos = 12, NodoActual = 0, columa = 0;
+            string NodoDato;
+            //nodo, relación nodo, peso
+            grafos g = new grafos(CantNodos);
+            g.ADDarista(1, 7, 2);
+            g.ADDarista(1, 4, 13);
+            g.ADDarista(4, 3, 10);
+            g.ADDarista(7, 6, 17);
+            g.ADDarista(7, 10, 8);
+            g.ADDarista(10, 9, 8);
+            g.ADDarista(6, 3, 11);
+            g.ADDarista(6, 9, 9);
+            g.ADDarista(3, 1, 25);
+            g.ADDarista(3, 2, 2);
+            g.ADDarista(3, 5, 30);
+            g.ADDarista(9, 5, 15);
+            g.ADDarista(5, 2, 1);
+            g.ADDarista(5, 8, 14);
+            g.ADDarista(8, 9, 3);
+            g.ADDarista(8, 11, 6);
+            g.ADDarista(11, 10, 7);
+
+            g.ShowMatriz();
+            //Pedimos los datos iniciales
+            Console.WriteLine("Inserte el Nodo Inicial: ");
+            Ninicio = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Inserte el Nodo Final: ");
+            Nfinal = int.Parse(Console.ReadLine());
+
+            //Se crea una tabla para poder guardar los datos 
+            int[,] table = new int[CantNodos, 3];
+            string[] datos = new string[] {"Visitado", "Distancia", "Previo"};
+            //se crea la tabla 
+            for (int i = 0; i < CantNodos; i++)
+            {
+                table[i, 0] = 0;
+                table[i, 1] = int.MaxValue;
+                table[i, 2] = 0;
+            }
+            //me cuesta "0" moverme a mi nodo inicio
+            table[Ninicio, 1] = 0;
+
+            //algoritmo
+            NodoActual = Ninicio;
+            do
+            {   //Colocamos el nodo en el que estamos en el apartado de "Visitado"
+                table[NodoActual, 0] = 1;
+                //for que recorre columna por columna la matriz 
+                for (columa = 0; columa < CantNodos; columa++)
+                {
+                    //Se busca el nodo al que está referenciado
+                    if (g.ValorFC(NodoActual, columa) != 0)
+                    {
+                        //se calcula la "distancia", se saca el peso y se le suma al nodo actual
+                        Distancia = g.ValorFC(NodoActual, columa) + table[NodoActual, 1];
+                        if(Distancia < table[columa, 1])
+                        {
+                            //colocamos el camino "más corto"
+                            table[columa, 1] = Distancia;
+                            //colocamos el nodo padre
+                            table[columa, 2] = NodoActual;
+                        }
+                    }
+                }
+                
+                int indiceMenor = -1;
+                int DisMenor = int.MaxValue;
+                //buscamos la menor distancia que haya del nuevo nodo en el que estamos
+                for(int j= 0; j < CantNodos; j++)
+                {
+                    if(table[j,1]<DisMenor && table[j, 0] == 0)
+                    {
+                        indiceMenor = j;
+                        DisMenor = table[j, 1];
+                    }
+                }
+                NodoActual = indiceMenor;
+
+            } while (NodoActual != -1);
+            
+
+            //Obtenemos la ruta más corta y menos pesada
+            List<int> camino = new List<int>();
+            int nodo = Nfinal;
+            //añadimos los nodos a la lista 
+            while (nodo != Ninicio)
+            {
+                camino.Add(nodo);
+                nodo = table[nodo, 2];
+            }
+            camino.Add(Ninicio);
+            //sacamos el camino más corto con el método Reverse
+            camino.Reverse();
+            foreach(int pos in camino)
+            {//imprimimos el camino más corto y de menor peso
+                Console.Write("{0}-->",pos);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ Algoritmo_Dijkstra/Algoritmo_Dijkstra/grafos.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algoritmo_Dijkstra
+{
+    class grafos
+    {
+        int[,] matriz; //matriz para visualizar 
+        int[] grado; // guarda el grado de los nodos
+        int nodos; // cantidad de nodos que hay en el grafo
+        //contructor
+        public grafos(int Nnodos)//recibe la cantidad de nodos para el grafo 
+        {
+            nodos = Nnodos;
+            //se crea la matriz
+            matriz = new int[nodos, nodos];
+
+            //se crea el arreglo donde se almacenan los grados
+            grado = new int[nodos];
+        }
+        //se crea una funación de añadir la arista, sin embargo como tenemos un grafo dirigido
+        //se le dan los parámetros de inicio y final
+        //Esto es la información del peso 
+        public void ADDarista(int NodoInicial, int NodoFinal, int PesoNodo)
+        {
+            matriz[NodoInicial, NodoFinal] = PesoNodo;
+        }
+        
+        public void ShowMatriz()
+        {
+            //se le da un color distinto para visualizar mejor la matriz
+            Console.ForegroundColor = ConsoleColor.Green;
+            //for para nada más visualizar la matriz 
+            for(int i = 0; i < nodos; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("\t{0}", i);
+            }
+            Console.WriteLine();
+            //for que muestra los datos de la matriz
+            for (int i = 0; i < nodos; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(i);
+                for(int j = 0; j < nodos; j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("\t{0}", matriz[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        //Esto es para saber que nodo está conectado con otro nodo 
+        public int ValorFC(int Fila, int Columna)
+        {
+            return matriz[Fila, Columna];
+        }
+        //Practicamente deslinda la conexión con otro grado que ya se "sacó" 
+        public void DecrementGrados(int Nodo)
+        {
+            grado[Nodo] = -1;
+
+            for(int i = 0; i < nodos; i++)
+            {
+                if (matriz[Nodo, i] == 1)
+                {
+                    grado[i]--;
+                }
+            }
+        }
+
+    }
+}
