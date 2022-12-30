f75bcd46c24e1123168c1429f621b23771e4a85f
Pedro Hernández <71133442+PedroHernandez654@users.noreply.github.com>
2022-12-30 17:21:34 -0600

    Delete Herencia directory

--- Herencia/Main.py
+++
@@ -1,8 +0,0 @@
-import Coche
-import bicicleta
-color=input("De que color es el auto? ")
-c= Coche.Coche(color,4,150,"V4")
-print(c.imprimir())
-tipo=input("Que tipo es la bicicleta?(Urbana, Deportiva)")
-b=bicicleta.Bicicleta("azul",2,tipo)
-print(b.imprimir())
--- Herencia/Vehiculo.py
+++
@@ -1,7 +0,0 @@
-class Vehiculo():
-    def __init__(self,color,rueda):
-        self.color=color
-        self.rueda=rueda
-
-    def imprimir(self):
-        return "El vehiculo es de color "+str(self.color)+" ,tiene "+str(self.rueda) + " ruedas "
