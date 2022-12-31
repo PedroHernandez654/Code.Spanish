eeb4c3880fa2fbef50a1d5d0508089655aea3431
Pedro Hernández <71133442+PedroHernandez654@users.noreply.github.com>
2022-12-30 18:00:40 -0600

    Add files via upload

---
+++ C#/Proyecto_Examen/Clases/Conductor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen2Parcial
+{
+    public class Conductor
+    {
+        int id_conductor;
+        int id_tipo,no_licencia;
+        string Usuario, Password, Conpassword, nombre, apellido_p, apellido_m, telefono;
+
+        public int Id_conductor { get => id_conductor; set => id_conductor = value; }
+        public int Id_tipo { get => id_tipo; set => id_tipo = value; }
+        public int No_licencia { get => no_licencia; set => no_licencia = value; }
+        public string Usuario1 { get => Usuario; set => Usuario = value; }
+        public string Password1 { get => Password; set => Password = value; }
+        public string Conpassword1 { get => Conpassword; set => Conpassword = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido_p { get => apellido_p; set => apellido_p = value; }
+        public string Apellido_m { get => apellido_m; set => apellido_m = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+    }
+}
---
+++ C#/Proyecto_Examen/Clases/vehiculo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen2Parcial
+{
+    public class vehiculo
+    {
+        int id_vehiculo, No_viajes, id_conductor;
+        string placas, tipo, marca, modelo;
+
+        public int Id_vehiculo { get => id_vehiculo; set => id_vehiculo = value; }
+        public int No_viajes1 { get => No_viajes; set => No_viajes = value; }
+        public int Id_conductor { get => id_conductor; set => id_conductor = value; }
+        public string Placas { get => placas; set => placas = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+    }
+}
